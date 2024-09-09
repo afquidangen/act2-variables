932e973447115b76f95260a94f3c8cfaced8e9ce
afquidangen <alyssafayequidangen@gmail.com>
2024-09-09 10:25:40 +0800

    Variables Activity 2

---
+++ variables.js
@@ -0,0 +1,16 @@
+let name = "Alyssa Faye";
+console.log(name);
+//output: Alyssa Faye
+
+let whatDoYouWannaBecomeInYourLife = "programmer";
+console.log(whatDoYouWannaBecomeInYourLife);
+//output: programmer
+
+let gender = "Female";
+console.log(gender);
+//output: Female
+
+let twitterHandle = "@uronurownquid";
+console.log(twitterHandle);
+//output: @uronurownquid
+
